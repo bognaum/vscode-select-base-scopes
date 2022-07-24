6766d330eb38268e0b5333a2e509521e0d290c50
Bognaum <bognaum@gmail.com>
2022-07-24 15:20:44 +0300

    Syntax example.

--- src/describe-syntax.ts
+++ src/describe-syntax.ts
@@ -14,26 +14,28 @@ const
 	slashed = seq(token("\\"), token(1)),
 	stringTag = domain("string.tag", seq(
 		token("${"),
-		// ref(() => mainScope)['*'].as("string.tag.content"),
-		nToken("}")['*'].as("string.tag"),
+		alt(
+			ref(() => mainScopeSubjects),
+			nToken("}"),
+		)['*'].as("string.tag.content"),
 		token("}"),
 	)),
 	string = domain("string", alt(
 		seq(
 			token("'"),
-			alt(nToken("'")['*'].merged(), slashed)['*'].as("string.content"),
+			alt(slashed, nToken("'"))['*'].as("string.content"),
 			token("'"),
 		).as("string.quoted"),
 		seq(
 			token('"'),
-			alt(nToken('"')['*'].merged(), slashed)['*'].as("string.content"),
+			alt(slashed, nToken('"'))['*'].as("string.content"),
 			token('"'),
 		).as("string.quoted"),
 		seq(
 			token('`'),
-			alt(stringTag, slashed, nToken('`')['*'])['*'],
+			alt(stringTag, slashed, nToken('`'))['*'].as("string.content"),
 			token('`'),
-		)
+		).as("string.quoted")
 	)),
 	commentLine = seq(
 		token("//"), 
@@ -46,19 +48,24 @@ const
 		token("*/")
 	),
 	comment = domain("comment", alt(commentLine, commentBlock)),
-	subjects = alt(
+	mainScopeSubjects = alt(
 		string,
 		comment,
 	),
 	main = alt(
-		subjects,
-		not(subjects)
+		mainScopeSubjects,
+		not(mainScopeSubjects)
 	)['*'].as("default"),
 	glob = global()(
-		subjects,
-		not(subjects)['*'].as("simple")
+		mainScopeSubjects,
+		not(mainScopeSubjects)['+'].as("simple")
 	);
 
 export default glob;
 
 console.log("Syntax analyzer compiled.");
+
+console.log("1", stringTag.applyToText("${el.scrollHeight}")?.namedOnly);
+console.log("1.1", stringTag.applyToText("${}")?.namedOnly);
+console.log("1.2", alt(mainScopeSubjects, token(1)).applyToText("el.scrollHeight")?.namedOnly);
+console.log("2", string.applyToText("'${el.scrollHeight}'")?.namedOnly);
